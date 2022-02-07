31ea2cbeecf8924f3a7bee0003c93a6a444f928c
Max Eliseev <mxseev@gmail.com>
2022-02-07 18:18:49 +0500

    chore: откат шрифта (не помогает)

--- tests/utils.ts
+++ tests/utils.ts
@@ -3,11 +3,6 @@ import {Page, expect} from "@playwright/test"
 const snapshot = async (page: Page, url: string, filename: string) => {
   await page.goto(url)
 
-  // wait for fonts loading
-  await page.waitForNavigation({
-    waitUntil: "networkidle"
-  })
-
   const screenshot = await page.screenshot()
   expect(screenshot).toMatchSnapshot(filename)
 }
