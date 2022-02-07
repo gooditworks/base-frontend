97a516d376325cc17d9e96452dfb4e227bebdddd
Max Eliseev <mxseev@gmail.com>
2022-02-07 18:01:16 +0500

    fix: проставлен шрифт, чтобы на разных платформах не отличались скришоты страниц в тестах PW

--- tests/snapshots/index.test.ts
+++ tests/snapshots/index.test.ts
@@ -1,6 +1,7 @@
 import {test} from "@playwright/test"
 import {snapshot} from "../utils"
 
+// force Playwright for same screenshot filenames on various platforms
 test.beforeEach(({page}, testInfo) => {
   // eslint-disable-next-line no-param-reassign
   testInfo.snapshotSuffix = "multi_platform"
--- tests/utils.ts
+++ tests/utils.ts
@@ -3,6 +3,9 @@ import {Page, expect} from "@playwright/test"
 const snapshot = async (page: Page, url: string, filename: string) => {
   await page.goto(url)
 
+  // wait for fonts loading
+  await page.waitForTimeout(2500)
+
   const screenshot = await page.screenshot()
   expect(screenshot).toMatchSnapshot(filename)
 }
