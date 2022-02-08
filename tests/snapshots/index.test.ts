4d25493f499b368328031e6d0b1065db32f6dec7
Max Eliseev <mxseev@gmail.com>
2022-02-08 16:36:19 +0500

    chore: удаление костыля, который менял snapshotSuffix

--- tests/snapshots/index.test.ts
+++ tests/snapshots/index.test.ts
@@ -1,10 +1,4 @@
 import {test} from "@playwright/test"
 import {snapshot} from "../utils"
 
-// force Playwright for same screenshot filenames on various platforms
-test.beforeEach(({page}, testInfo) => {
-  // eslint-disable-next-line no-param-reassign
-  testInfo.snapshotSuffix = "multi_platform"
-})
-
 test("index page snapshot", ({page}) => snapshot(page, "/", "index.png"))
