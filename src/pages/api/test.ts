a7518184ef1bcb07219f7192ca8734e0b5985018
Max Eliseev <mxseev@gmail.com>
2022-07-07 21:47:11 +0500

    chore: sentry.captureexception в ручную

--- src/pages/api/test.ts
+++ src/pages/api/test.ts
@@ -1,9 +1,9 @@
 import {NextApiHandler} from "next"
-import {withSentry} from "@gooditworks/monitoring/next"
+import {Sentry, withSentry} from "@gooditworks/monitoring/next"
 
 const testHandler: NextApiHandler = (request, response) => {
   if (request.query.throw) {
-    throw new Error(`triggered test exception: ${request.query.throw}`)
+    Sentry.captureException(new Error(`triggered test exception: ${request.query.throw}`))
   }
 
   return response.status(200).json({success: true})
