51e95d93d7ad388882a818cfe137376f591c2995
Max Eliseev <mxseev@gmail.com>
2022-11-22 20:08:16 +0700

    fix: пофикшено правило commitlint, теперь можно создавать коммиты типа wip

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,6 +2,6 @@ module.exports = {
   extends: ["@commitlint/config-conventional"],
   plugins: ["commitlint-plugin-function-rules"],
   rules: {
-    "type-enum": [2, "always", ["feat", "fix", "docs", "style", "refactor", "test", "revert", "chore", "WIP"]]
+    "type-enum": [2, "always", ["feat", "fix", "docs", "style", "refactor", "test", "revert", "chore", "wip"]]
   }
 }
