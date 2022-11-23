33c1ae4fc344346be0278c0d4b2b7ad752fb02ec
Max Eliseev <mxseev@gmail.com>
2022-11-23 15:45:36 +0700

    chore: конфиг commitzen обновлен из PR про инструменты

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,6 +2,6 @@ module.exports = {
   extends: ["@commitlint/config-conventional"],
   plugins: ["commitlint-plugin-function-rules"],
   rules: {
-    "type-enum": [2, "always", ["feat", "fix", "docs", "style", "refactor", "test", "revert", "chore", "wip"]]
+    "type-enum": [2, "always", ["feat", "fix", "docs", "refactor", "revert", "chore", "wip"]]
   }
 }
