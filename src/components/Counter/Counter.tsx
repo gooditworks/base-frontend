8cd1feb522e81c01028b4df120097a1c51ba60cd
Max Eliseev <mxseev@gmail.com>
2022-11-24 13:05:00 +0700

    refactor: удалены import React

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -1,5 +1,3 @@
-import React from "react"
-
 interface Props {
   value: number
   onIncrement: () => void
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,3 @@
-import React from "react"
-
 import useCounter from "hooks/useCounter"
 import Counter from "components/Counter"
 
