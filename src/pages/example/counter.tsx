adc78da177fbc7433c62d37c23935f9eb3f9c402
Max Eliseev <mxseev@gmail.com>
2022-11-24 13:17:08 +0700

    chore: демонстрация компонента Counter перенесена в /examples

---
+++ src/pages/example/counter.tsx
@@ -0,0 +1,20 @@
+import {FC} from "react"
+
+import useCounter from "hooks/useCounter"
+import Counter from "components/Counter"
+
+const CounterPage: FC = () => {
+  const {value, increment, decrement} = useCounter()
+
+  if (value === 42) {
+    throw new Error("counter value is 42!")
+  }
+
+  return (
+    <div className="w-screen h-screen flex items-center justify-center">
+      <Counter value={value} onIncrement={increment} onDecrement={decrement} />
+    </div>
+  )
+}
+
+export default CounterPage
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,5 @@
-import useCounter from "hooks/useCounter"
-import Counter from "components/Counter"
+import {FC} from "react"
 
-const Index = () => {
-  const {value, increment, decrement} = useCounter()
+const IndexPage: FC = () => <h2>TODO: put README there</h2>
 
-  if (value === 42) {
-    throw new Error("counter value is 42!")
-  }
-
-  return (
-    <div className="w-screen h-screen flex items-center justify-center">
-      <Counter value={value} onIncrement={increment} onDecrement={decrement} />
-    </div>
-  )
-}
-
-export default Index
+export default IndexPage
