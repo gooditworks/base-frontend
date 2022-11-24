04ab2bccf00b6f3b745a0096e08b599c89c6b6e7
Max Eliseev <mxseev@gmail.com>
2022-11-24 13:46:43 +0700

    fix: пофикшены e2e тесты

--- tests/started.test.ts
+++ tests/started.test.ts
@@ -3,7 +3,7 @@ import {test, expect} from "@playwright/test"
 // Checking if the server has started (basic smoke test)
 test("server started correctly", async ({page}) => {
   await page.goto("/")
-  const counterValue = page.locator("data-test=counter_value")
+  const headerValue = page.locator("h2")
 
-  expect(counterValue).toHaveText("0")
+  expect(headerValue).toContainText("TODO")
 })
