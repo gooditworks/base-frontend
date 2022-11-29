ce8d4cb717fdfb1c89091e6847b13561e33ad80c
Max Eliseev <mxseev@gmail.com>
2022-11-29 07:22:25 +0700

    feat: чуть более красивая страница ошибки

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -13,9 +13,12 @@ const ErrorPage: FC<Props> = ({error}) => {
   }, [error])
 
   return (
-    <div>
-      <h2>Something went wrong!</h2>
-      <pre>{error.message}</pre>
+    <div className="p-8 w-screen h-screen flex flex-col items-center justify-center">
+      <h2 className="text-5xl font-bold">Something went wrong!</h2>
+      <p className="mt-8">
+        We have already been notified of the problem and will resolve it shortly, please
+        try again later.
+      </p>
     </div>
   )
 }
